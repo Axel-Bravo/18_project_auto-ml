af22fdc25c19456bf3a2e445e2f135d3bd9520d1
Axel_Bravo <axel.bravo@protonmail.com>
2018-07-26 19:38:21 +0200

    Unitest refactoring
    TestLoad

--- test_auto_ml.py
+++ test_auto_ml.py
@@ -5,132 +5,43 @@ from pandas.util.testing import assert_frame_equal
 import auto_ml as ml
 test_path = 'test/'
 
-class TestLoadFromFile(unittest.TestCase):
 
-    def test_all_data_ok(self):
-        """
-        Function behaves properly on desired cases
-        :return: AutoClf
-            ._train: {pd.DataFrame}: train data
-            ._test: {pd.DataFrame}: test data
-        """
+class TestLoad(unittest.TestCase):
+    def setUp(self):
         data_train = pd.read_csv(test_path + 'load_from_file_ok' + '.csv')
         data_test = pd.read_csv(test_path + 'load_from_file_ok_2' + '.csv')
-        train_data = test_path + 'load_from_file_ok' + '.csv'
-        test_data = test_path + 'load_from_file_ok_2' + '.csv'
+        path_train = test_path + 'load_from_file_ok_2' + '.csv'
+        path_test = test_path + 'load_from_file_ok_2' + '.csv'
+        self.data_test_pass = [(data_train, data_test), (path_train, path_test)]
 
-        clf_ml = ml.AutoClf()
-        clf_ml.load(train_data=train_data, test_data=test_data)
+        data_train_type = pd.read_csv(test_path + 'load_from_file_nok_col_type' + '.csv')
+        path_train_type = test_path + 'load_from_file_nok_col_type' + '.csv'
+        data_train_y = pd.read_csv(test_path + 'load_from_file_nok_y' + '.csv')
+        path_train_y = test_path + 'load_from_file_nok_y' + '.csv'
+        data_test_type = pd.read_csv(test_path + 'load_from_file_nok_col_type' + '.csv')
+        path_test_type = test_path + 'load_from_file_nok_col_type' + '.csv'
+        data_test_y = pd.read_csv(test_path + 'load_from_file_nok_y' + '.csv')
+        path_test_y = test_path + 'load_from_file_nok_y' + '.csv'
+        self.data_test_fail = [(data_train_type, data_test), (path_train_type, path_test), (data_train_y, data_test),
+                               (path_train_y, path_test), (data_train, data_test_y), (path_train, path_test_y),
+                               (data_train, data_test_type), (path_train, path_test_type)]
 
-        assert_frame_equal(data_train, clf_ml._train)
-        assert_frame_equal(data_test, clf_ml._test)
+    def test_data_load_ok(self):
+        for train, test in self.data_test_pass:
+            clf_ml = ml.AutoClf()
+            clf_ml.load(train, test)
 
-    def test_column_types_train_data(self):
-        """
-        Check that only numerical data is accepted
-        :return: None
-        """
-        train_data = test_path + 'load_from_file_nok_col_type' + '.csv'
-        test_data = test_path + 'load_from_file_ok_2' + '.csv'
+            if type(train) is str:
+                assert_frame_equal(pd.read_csv(train), clf_ml._train)
+                assert_frame_equal(pd.read_csv(test), clf_ml._test)
+            else:
+                assert_frame_equal(train, clf_ml._train)
+                assert_frame_equal(test, clf_ml._test)
 
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, train_data, test_data)
-
-    def test_column_types_test_data(self):
-        """
-        Check that only numerical data is accepted
-        :return: None
-        """
-        train_data = test_path + 'load_from_file_ok_2' + '.csv'
-        test_data = test_path + 'load_from_file_nok_col_type' + '.csv'
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, train_data, test_data)
-
-    def test_column_names_train_data(self):
-        """
-        Check the column naming is appropriate, i.e. there is a column named "Y"
-        :return: None
-        """
-        train_data = test_path + 'load_from_file_nok_y' + '.csv'
-        test_data = test_path + 'load_from_file_ok_2' + '.csv'
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, train_data, test_data)
-
-    def test_column_names_test_data(self):
-        """
-        Check the column naming is appropriate, i.e. there is a column named "Y"
-        :return: None
-        """
-        train_data = test_path + 'load_from_file_ok_2' + '.csv'
-        test_data = test_path + 'load_from_file_nok_y' + '.csv'
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, train_data, test_data)
-
-
-class TestLoadFromMemory(unittest.TestCase):
-
-    def test_all_data_ok(self):
-        """
-        Function behaves properly on desired cases
-        :return: AutoClf
-            ._train: {pd.DataFrame}: train data
-            ._test: {pd.DataFrame}: test data
-        """
-        data_train = pd.read_csv(test_path + 'load_from_file_ok' + '.csv')
-        data_test = pd.read_csv(test_path + 'load_from_file_ok_2' + '.csv')
-
-        clf_ml = ml.AutoClf()
-        clf_ml.load(train_data=data_train, test_data=data_test)
-
-        assert_frame_equal(data_train, clf_ml._train)
-        assert_frame_equal(data_test, clf_ml._test)
-
-    def test_column_types_train_data(self):
-        """
-        Check that only numerical data is accepted
-        :return: None
-        """
-        data_train = pd.read_csv(test_path + 'load_from_file_nok_col_type' + '.csv')
-        data_test = pd.read_csv(test_path + 'load_from_file_ok_2' + '.csv')
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, data_train, data_test)
-
-    def test_column_types_test_data(self):
-        """
-        Check that only numerical data is accepted
-        :return: None
-        """
-        data_train = pd.read_csv(test_path + 'load_from_file_ok_2' + '.csv')
-        data_test = pd.read_csv(test_path + 'load_from_file_nok_col_type' + '.csv')
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, data_train, data_test)
-
-    def test_column_names_train_data(self):
-        """
-        Check the column naming is appropriate, i.e. there is a column named "Y"
-        :return: None
-        """
-        data_train = pd.read_csv(test_path + 'load_from_file_nok_y' + '.csv')
-        data_test = pd.read_csv(test_path + 'load_from_file_ok_2' + '.csv')
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, data_train, data_test)
-
-    def test_column_names_test_data(self):
-        """
-        Check the column naming is appropriate, i.e. there is a column named "Y"
-        :return: None
-        """
-        data_train = pd.read_csv(test_path + 'load_from_file_ok_2' + '.csv')
-        data_test = pd.read_csv(test_path + 'load_from_file_nok_y' + '.csv')
-
-        clf_ml = ml.AutoClf()
-        self.assertRaises(Exception, clf_ml.load, data_train, data_test)
+    def test_data_load_failed(self):
+        for train, test in self.data_test_fail:
+            clf_ml = ml.AutoClf()
+            self.assertRaises(Exception, clf_ml.load, train, test)
 
 
 if __name__ == '__main__':
